16ca6f62404f4d85e4b68c90e804afd027d13fc1
Desec <48061253+DATA-GaMi@users.noreply.github.com>
2025-01-06 17:41:17 +0800

    Add project Instead-PowerShell

---
+++ Instead-PowerShell/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace Instead_PowerShell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(args[0]);
+            try
+            {
+                using (PowerShell powerShell = PowerShell.Create())
+                {
+                    powerShell.AddScript("Invoke-Expression " + args[0]);
+
+                    Collection<PSObject> results = powerShell.Invoke();
+
+                    foreach (PSObject result in results)
+                    {
+                        Console.WriteLine(result.ToString());
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invoke needs param");
+                Console.ResetColor();
+            }
+        }
+    }
+}
